d5f00b5bb49d0c7d4c8d5097893b92e422095a1b
Ivan Lazarevic <24551171+ivanbgd@users.noreply.github.com>
2024-10-12 00:47:39 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,24 @@
 
 use std::env;
 
-use serde_bencode;
-use serde_json;
+use anyhow::Result;
 
 use bittorrent_starter_rust::decode::decode_bencoded_value;
 
-fn main() {
+fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
 
     match command.as_str() {
         "decode" => {
             let encoded_value = &args[2];
-            let decoded_value = decode_bencoded_value(encoded_value);
+            let decoded_value = decode_bencoded_value(encoded_value)?;
             println!("{}", decoded_value);
         }
         _ => {
             println!("Unknown command: {}", args[1]);
         }
     }
+
+    Ok(())
 }
