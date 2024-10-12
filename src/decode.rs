3c2b66837c42f280645774422537eaee3c98e3ef
Ivan Lazarevic <24551171+ivanbgd@users.noreply.github.com>
2024-10-12 03:27:09 +0200

    decode `bencoded` lists

--- src/decode.rs
+++ src/decode.rs
@@ -3,18 +3,30 @@ use serde_bencode;
 use serde_json;
 
 pub fn decode_bencoded_value(encoded_value: &str) -> Result<serde_json::Value> {
-    let first = encoded_value.chars().next().unwrap();
-    if first.is_ascii_digit() {
-        // if encoded_value starts with a digit, it's a string
-        Ok(serde_json::Value::String(serde_bencode::from_str(
-            encoded_value,
-        )?))
-    } else if first == 'i' {
-        // if encoded_value starts with the letter 'i', it's a number
-        Ok(serde_json::Value::Number(serde_bencode::from_str(
-            encoded_value,
-        )?))
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value);
+    fn decode(value: serde_bencode::value::Value) -> Result<serde_json::Value> {
+        match value {
+            serde_bencode::value::Value::Bytes(string) => {
+                let string = String::from_utf8(string)?;
+                Ok(serde_json::Value::String(string))
+            }
+            serde_bencode::value::Value::Int(integer) => {
+                let integer = serde_json::Number::from(integer);
+                Ok(serde_json::Value::Number(integer))
+            }
+            serde_bencode::value::Value::List(list) => {
+                let array = list
+                    .into_iter()
+                    .map(decode)
+                    .collect::<Result<Vec<serde_json::Value>>>()?;
+                Ok(serde_json::Value::Array(array))
+            }
+            _ => {
+                panic!("Unhandled encoded value: {:?}", value);
+            }
+        }
     }
+
+    let value = serde_bencode::from_str(encoded_value)?;
+
+    decode(value)
 }
