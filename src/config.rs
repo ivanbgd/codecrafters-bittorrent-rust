8dd4f5230c3a5e1110e66d6ef9545cd84371cdd4
Ivan Lazarevic <24551171+ivanbgd@users.noreply.github.com>
2024-11-23 02:03:41 +0100

    Improve app config to use default values whenever possible, i.e., to panic only when the file isn't formatted properly

--- src/config.rs
+++ src/config.rs
@@ -2,10 +2,15 @@
 //!
 //! Configuration used throughout the application
 //!
-//! The function `get_config` tries to read and parse the optional "config.json" file.
+//! The function [`get_config`] tries to read and parse the optional "config.json" file.
+//!
 //! If it succeeds, it returns the configuration from the JSON file.
+//!
 //! If it fails to find the file, it uses default values from [`crate::constants`].
-//! If the file isn't formatted properly, or if a field is missing, the function will panic.
+//!
+//! If the file is missing a field, the field will be assigned its default value from [`crate::constants`].
+//!
+//! If the file isn't formatted properly, the function will panic.
 
 use std::fs::File;
 
@@ -20,12 +25,25 @@ pub struct Config {
     pub max_pipelined_requests: usize,
 }
 
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            max_num_peers: MAX_NUM_PEERS,
+            max_pipelined_requests: MAX_PIPELINED_REQUESTS,
+        }
+    }
+}
+
 /// Tries to read and parse the optional "config.json" file.
+///
 /// If it succeeds, it returns the configuration from the JSON file.
+///
 /// If it fails to find the file, it uses default values from [`crate::constants`].
 ///
+/// If the file is missing a field, the field will be assigned its default value from [`crate::constants`].
+///
 /// # Panics
-/// If the file isn't formatted properly, or if a field is missing, the function will panic.
+/// If the file isn't formatted properly, the function will panic.
 pub fn get_config() -> Config {
     match File::open(CONFIG_FILE_PATH) {
         Ok(file) => {
@@ -33,23 +51,16 @@ pub fn get_config() -> Config {
                 panic!("{CONFIG_FILE_PATH} is not formatted properly: {err}")
             });
             Config {
-                max_num_peers: serde_json::from_value(
-                    json.get("max_num_peers")
-                        .expect("missing the max_num_peers key")
-                        .clone(),
-                )
-                .unwrap(),
-                max_pipelined_requests: serde_json::from_value(
-                    json.get("max_pipelined_requests")
-                        .expect("missing the max_pipelined_requests key")
-                        .clone(),
-                )
-                .unwrap(),
+                max_num_peers: match json.get("max_num_peers") {
+                    Some(v) => serde_json::from_value(v.clone()).unwrap_or(MAX_NUM_PEERS),
+                    None => MAX_NUM_PEERS,
+                },
+                max_pipelined_requests: match json.get("max_pipelined_requests") {
+                    Some(v) => serde_json::from_value(v.clone()).unwrap_or(MAX_PIPELINED_REQUESTS),
+                    None => MAX_PIPELINED_REQUESTS,
+                },
             }
         }
-        Err(_) => Config {
-            max_num_peers: MAX_NUM_PEERS,
-            max_pipelined_requests: MAX_PIPELINED_REQUESTS,
-        },
+        Err(_) => Config::default(),
     }
 }
